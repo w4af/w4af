ed7b03158f09d8dee465589bafb99983da23203e
Arthur Taylor <arthur@codders.de>
2022-02-11 17:18:14 +0100

    Don't disable retirejs check

--- w3af/core/controllers/dependency_check/external/retirejs.py
+++ w3af/core/controllers/dependency_check/external/retirejs.py
@@ -31,7 +31,6 @@ def retirejs_is_installed():
     """
     :return: True if retirejs is installed and we were able to parse the version.
     """
-    return True
     try:
         version = subprocess.check_output('npx retire --version', shell=True)
     except subprocess.CalledProcessError:
