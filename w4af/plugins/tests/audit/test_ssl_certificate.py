5f751a71ad010c8d533fb8017e2a4515b54bdbab
security-companion <mail@security-companion.net>
2023-02-16 16:05:52 +0100

    add missing import

--- w4af/plugins/tests/audit/test_ssl_certificate.py
+++ w4af/plugins/tests/audit/test_ssl_certificate.py
@@ -19,6 +19,7 @@ along with w4af; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 import os
+from datetime import datetime
 
 import pytest
 
