629ce01261edc30362f3fc9bc0c74cc6c1907300
security-companion <mail@security-companion.net>
2023-09-06 21:18:25 +0200

    remove old sh files

--- w4af/core/controllers/ci/install_scripts/install_core_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-# We now install the core dependencies for the w4af project.
-python -c 'from w4af.core.controllers.dependency_check.dependency_check import dependency_check;dependency_check()'
-
-if [ -f requirements.txt ]; then
-	pip install -r requirements.txt;
-fi
-
